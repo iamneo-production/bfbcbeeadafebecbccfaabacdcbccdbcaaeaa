ed355f0ecd1095993fe4da0fac67c1a9f2dd22dc
Iamneo <techadmin@iamneo.ai>
2023-08-05 05:35:35 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -2,6 +2,7 @@ package com.examly.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class SpringappApplication {
 
--- springapp/src/test/java/com/examly/springapp/SpringApplicationTests.java
+++ springapp/src/test/java/com/examly/springapp/SpringApplicationTests.java
@@ -1,66 +1,56 @@
-package com.examly.springapp;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.net.URL;
-import org.junit.Test; 
-import org.junit.runner.RunWith;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.interactions.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.WebServer;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
-@SpringBootTest(classes = SpringappApplication.class)
-@AutoConfigureMockMvc
-@RunWith(SpringRunner.class)
-public class SpringApplicationTests<ChromeOptions> {
+@Configuration
+@ContextConfiguration(classes = AppTest.class)
+public class AppTest {
 
-	@Autowired
-    private MockMvc mockMvc;	
+    @Autowired
+    private WebDriver driver;
 
-	ChromeOptions chromeOptions = new ChromeOptions();
-	WebServer driver = null;
+    @BeforeTest
+    public void beforeTest() {
+        driver.manage().window().maximize();
+    }
 
-	@BeforeTest
-	public void beforeTest() throws Exception {
-		
-		driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
-	}
+    @Test
+    public void testcase_1() throws InterruptedException {
+        driver.navigate().to("https://jqueryui.com/droppable/");
+    }
 
-	@Test
-	public void testcase_1() throws InterruptedException 
-{
-		driver.navigate().to("https://jqueryui.com/droppable/");
-		
-		
-		
-	}
+    @Test
+    public void testcase_2() throws InterruptedException {
+        driver.switchTo().frame(0);
+        WebElement drag = driver.findElement(By.id("draggable"));
+        WebElement drop = driver.findElement(By.id("droppable"));
+        Actions a = new Actions(driver);
+        a.dragAndDrop(drag, drop).build().perform();
+    }
 
-	@Test
-	public void testcase_2() throws InterruptedException 
-      {
-		driver.switchTo().frame(0);
-	    WebElement drag= driver.findElement(By.id("draggable"));
-	    WebElement drop=driver.findElement(By.id("droppable"));
-		Actions a=new Actions(driver);
-	    a.dragAndDrop(drag, drop).build().perform();
-	}
-	@Test
-	public void testcase_3() throws InterruptedException 
-      {
-		WebElement drop=driver.findElement(By.id("droppable"));
-		Assert.assertEquals(drop.getText(),"Dropped!");
-	}
+    @Test
+    public void testcase_3() throws InterruptedException {
+        WebElement drop = driver.findElement(By.id("droppable"));
+        Assert.assertEquals(drop.getText(), "Dropped!");
+    }
 
-		
-	@AfterTest
-	public void afterTest() {
-		driver.quit();
-	}
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
+
+    @Bean
+    public WebDriver getWebDriver() {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "http://localhost:8080");
+        return new ChromeDriver();
+    }
 }
